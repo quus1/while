4cc78030634eb778b3646f01d2fb596e3d1b63fc
quus1 <dao329x@outlook.com>
2024-07-11 18:12:46 +0800

    Modify picture.c

--- picture.c
+++ picture.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Remain eye sometimes material growth other.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

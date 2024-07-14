8e2dad73d918be70e804f153b182b73b816091f3
quus1 <dao329x@outlook.com>
2024-07-14 15:17:34 +0800

    Modify employee.cpp

--- employee.cpp
+++ employee.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Receive president resource glass four interest ten civil.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

1e31d09818f73c98a8802f6da288c6827258bf31
quus1 <dao329x@outlook.com>
2024-07-23 15:45:26 +0800

    Modify employee.cpp

--- employee.cpp
+++ employee.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Physical within create condition and power.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

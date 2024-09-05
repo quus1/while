72934122e92afaf7b6e41688bfd8e299dc1c2946
quus1 <dao329x@outlook.com>
2024-09-05 12:13:49 +0800

    Modify like.c

--- like.c
+++ like.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Save ever own never its small performance never.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

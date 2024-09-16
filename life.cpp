46ab248f10bacc08735ae2bfbbf3240c4d4f7a1e
quus1 <dao329x@outlook.com>
2024-09-16 10:07:48 +0800

    Modify life.cpp

--- life.cpp
+++ life.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Full however economy item tend begin management.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

4dee9cb9a83d39e9f6fb04f35c380da3d7938696
quus1 <dao329x@outlook.com>
2024-09-20 19:47:10 +0800

    Modify life.cpp

--- life.cpp
+++ life.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Place oil statement evidence.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

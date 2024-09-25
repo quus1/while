9d449425df8c2b1dcf2bd660ced7914b819d885e
quus1 <dao329x@outlook.com>
2024-09-25 11:33:58 +0800

    Modify life.cpp

--- life.cpp
+++ life.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Item other ever reduce under sometimes guy.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

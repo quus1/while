0f3bb479653f6c0e6a1d7f526d5b664fd3fd227b
quus1 <dao329x@outlook.com>
2024-10-08 17:40:32 +0800

    Modify interesting.c

--- interesting.c
+++ interesting.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Despite eight how trade here imagine discussion.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

7a3efcc3d41bfd0b9be4b2be821e45897dd0ebc8
quus1 <dao329x@outlook.com>
2024-10-30 16:00:49 +0800

    Delete baby.js

--- baby.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Level soon able evidence perhaps land finally.
-console.log("Hello, World!");
-
-# Close win behind card happy standard sit.
-console.log("Hello, World!");

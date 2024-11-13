f58458c2444cff76d17e97b09d57d01030a38cac
quus1 <dao329x@outlook.com>
2024-11-13 14:46:27 +0800

    Modify who.c

--- who.c
+++ who.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Media camera pass his war card standard.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

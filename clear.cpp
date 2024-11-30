511552b39e9b98f93b27cd3afcc07387078dad3a
quus1 <dao329x@outlook.com>
2024-11-30 23:56:53 +0800

    Modify clear.cpp

--- clear.cpp
+++ clear.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# How main purpose house.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

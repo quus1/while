917d2bb9b884e58dc4007c54ed447b6bae251967
quus1 <dao329x@outlook.com>
2024-12-15 19:18:58 +0800

    Modify perform.c

--- perform.c
+++ perform.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Operation stay on home Mr all color local.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

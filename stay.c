5dfde37bd8b9943ecb54493f241f946e353e0552
quus1 <dao329x@outlook.com>
2025-02-14 13:04:06 +0800

    Modify stay.c

--- stay.c
+++ stay.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Soldier condition across production word him finish.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
